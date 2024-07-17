b2bc6457b898b1455f076dc7bd6dfe97124aafa1
Muhammad Ainur Rofiq <rofiq@192.168.1.2>
2024-07-18 02:46:47 +0700

    middlewares init

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+const { sessionConfig } = require('./middlewares/auth');
 const express = require('express');
 const app = express();
 const port = 3000;
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
+app.use(sessionConfig)
+
 app.use('/', require('./routes'))
 
 app.listen(port, () => console.log('service is running on http://localhost:3000/'))
