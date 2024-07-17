81d5a952c86ec68af81c447f1afffb62867fee55
Muhammad Ainur Rofiq <rofiq@192.168.1.2>
2024-07-17 17:41:14 +0700

    update assosiation on models

--- models/course.js
+++ models/course.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Course.belongsTo(models.Category)
       Course.belongsToMany(models.User, { through: models.UserCourse });
     }
   }
