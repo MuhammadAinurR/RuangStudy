8ca2e960236c72dcb7091ff4b90eedf2aff1d714
Muhammad Ainur Rofiq <rofiq@192.168.1.2>
2024-07-18 13:13:45 +0700

    add

--- routes/user.js
+++ routes/user.js
@@ -6,6 +6,9 @@ router.get('/register', UserController.register)
 router.get('/login', UserController.login)
 router.post('/login', UserController.loginPost)
 
+
+router.get('/courses')
+
 // dummy login
 router.use((req, res, next) => {
     req.session.user = { id: 1, email: "ainurmoh@gmail.com", role: "SuperUser", image: "https://as2.ftcdn.net/v2/jpg/00/81/49/73/1000_F_81497385_G0PLVpeTpxNmMpmrd1X5ZhcBeNuYEfdK.jpg" };
