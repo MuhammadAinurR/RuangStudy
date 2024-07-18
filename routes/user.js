fb58678339ddf1d44c00676f7090fdb8f59b7938
Muhammad Ainur Rofiq <rofiq@192.168.1.2>
2024-07-18 13:50:09 +0700

    update login register form

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,5 @@
 const router = require('express').Router()
+const PublicController = require('../controllers/public');
 const UserController = require('../controllers/user');
 const { isLoggedin }= require('../middlewares/auth');
 
@@ -7,20 +8,20 @@ router.get('/login', UserController.login)
 router.post('/login', UserController.loginPost)
 
 
-router.get('/courses')
 
+router.get('/courses', PublicController.courses)
 // dummy login
-router.use((req, res, next) => {
-    req.session.user = { id: 1, email: "ainurmoh@gmail.com", role: "SuperUser", image: "https://as2.ftcdn.net/v2/jpg/00/81/49/73/1000_F_81497385_G0PLVpeTpxNmMpmrd1X5ZhcBeNuYEfdK.jpg" };
-    next()
-})
+// router.use((req, res, next) => {
+//     req.session.user = { id: 1, email: "ainurmoh@gmail.com", role: "SuperUser", image: "https://as2.ftcdn.net/v2/jpg/00/81/49/73/1000_F_81497385_G0PLVpeTpxNmMpmrd1X5ZhcBeNuYEfdK.jpg" };
+//     next()
+// })
 
 // login middleware
 router.use(isLoggedin)
+router.get('/course/:id', UserController.course)
 router.get('/dashboard', UserController.dashboard)
 router.get('/academy', UserController.academy)
 router.get('/signout', UserController.logOut)
-router.get('/course/:id', UserController.course)
 router.get('/category/:id', UserController.category)
 
 module.exports = router;
