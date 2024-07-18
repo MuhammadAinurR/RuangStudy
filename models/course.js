d8f96c93582f1f3e0d2973f60449cb4454b6b9bb
Vincent <vincentcan909@gmail.com>
2024-07-19 00:36:57 +0700

    Add Validation

--- models/course.js
+++ models/course.js
@@ -16,11 +16,69 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   Course.init({
-    CategoryId: DataTypes.INTEGER,
-    name: DataTypes.STRING,
-    description: DataTypes.STRING,
-    image: DataTypes.STRING,
-    jumlahModul: DataTypes.INTEGER
+    CategoryId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Please Select Course Category!'
+        },
+        notEmpty: {
+          msg: 'Please Select Course Category!'
+        }
+      }
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Course Title is Required!'
+        },
+        notEmpty: {
+          msg: 'Course Title is Required!'
+        }
+      }
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Course Description is Required!'
+        },
+        notEmpty: {
+          msg: 'Course Description is Required!'
+        }
+      }
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Image Url is Required!'
+        },
+        notEmpty: {
+          msg: 'Image Url is Required!'
+        },
+        isUrl: {
+          msg: 'Only Input Image Url!'
+        }
+      }
+    },
+    jumlahModul: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Jumlah Modul is Required!'
+        },
+        notEmpty: {
+          msg: 'Jumlah Modul is Required!'
+        }
+      }
+    }
   }, {
     sequelize,
     modelName: 'Course',
